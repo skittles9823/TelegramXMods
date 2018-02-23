0c8950335912be43c9d7e154bac75ba260633d30
skittles9823 <skat1@live.com.au>
2018-02-24 00:07:05 +1100

    Add changelog

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -4,6 +4,7 @@ cp app/build/outputs/apk/debug/app-debug.apk fuckdis.apk
  
 curl -F chat_id="427673272" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="-1001220326034" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="1386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
  
  
