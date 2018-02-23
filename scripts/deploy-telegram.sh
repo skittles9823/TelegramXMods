67d9eaf09a550192820b58dbbbe963eedb77819b
skittles9823 <skat1@live.com.au>
2018-02-24 01:34:54 +1100

    Fexed da fingo :D

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 cp app/build/outputs/apk/debug/app-debug.apk TelegramXMods-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="-1386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="-1386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="-1001386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 
 echo $(./scripts/changelog.sh)
