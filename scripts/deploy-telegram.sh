70cb6a4ce54fd6659ce0cb18a5fb2b6579e40db3
skittles9823 <skat1@live.com.au>
2018-02-23 23:45:16 +1100

    We got a channel now?

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 cp app/build/outputs/apk/debug/app-debug.apk fuckdis.apk
+
  
 curl -F chat_id="427673272" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="-1001220326034" -F document=@"fuckdis.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="1386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
  
  
