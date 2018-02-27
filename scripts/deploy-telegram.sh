8d653eb437b151ceef74514b1eb161bd79666508
Skittles9823 <skat1@live.com.au>
2018-02-27 21:48:15 +1100

    Maybi nao?

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cp app/build/outputs/apk/app-release*.apk TelegramXMods-$TRAVIS_BUILD_NUMBER.apk
+cp app/build/outputs/apk/app-release.apk TelegramXMods-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
