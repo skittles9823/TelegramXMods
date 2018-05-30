luisbocanegra/substratum-tweaks

Release 13.0.0: Remove NDK dependency and consolidate constants
Many themers have been complaining about the NDK and the lack of consolidation regarding functions and things they have to change.

The template also had to be cleaned out of some redundant variables.

In this new update, we have changed everything to three files:

app/ThemerConstants.gradle (for everything AP related)
app/src/main/kotlin/substratum/theme/template/AdvancedConstants.kt (optional)
app/src/main/res/values/theme_configurations.xml (for theme info)

This means you can remove the dependencies for compiling C++ without regression.

Signed-off-by: Nicholas Chum <nicholas@prjkt.io>

app/src/main/kotlin/substratum/theme/template/ThemeFunctions.kt
@@ -9,13 +9,11 @@ import android.content.pm.Signature
 import android.net.Uri
 import android.os.RemoteException
 import android.widget.Toast
-import substratum.theme.template.Constants.BLACKLISTED_APPLICATIONS
-import substratum.theme.template.Constants.ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS
-import substratum.theme.template.Constants.MINIMUM_SUBSTRATUM_VERSION
-import substratum.theme.template.Constants.OTHER_THEME_SYSTEMS
+import substratum.theme.template.AdvancedConstants.BLACKLISTED_APPLICATIONS
+import substratum.theme.template.AdvancedConstants.MINIMUM_SUBSTRATUM_VERSION
+import substratum.theme.template.AdvancedConstants.OTHER_THEME_SYSTEMS
 
-
-@Suppress("ConstantConditionIf") // This needs to be defined by the themer, so suppress!
+@Suppress("ConstantConditionIf")
 object ThemeFunctions {
 
     val SUBSTRATUM_PACKAGE_NAME = "projekt.substratum"
@@ -40,7 +38,7 @@ object ThemeFunctions {
             if (pi.signatures != null
                     && pi.signatures.size == 1
                     && ((SIGNATURES[0] == pi.signatures[0]) ||
-                    (SIGNATURES[1] == pi.signatures[0]))) {
+                            (SIGNATURES[1] == pi.signatures[0]))) {
                 return true
             }
             false
@@ -90,7 +88,7 @@ object ThemeFunctions {
     }
 
     fun getSelfVerifiedIntentResponse(context: Context): Int? {
-        return if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        return if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             getSelfSignature(context)
         } else {
             getSubstratumSignature(context)
@@ -118,7 +116,7 @@ object ThemeFunctions {
 
     fun getSelfVerifiedThemeEngines(context: Context): Boolean? {
         val isPermitted: Boolean? = OTHER_THEME_SYSTEMS.any { isPackageInstalled(context, it) }
-        if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             return isPermitted
         } else if (isPackageInstalled(context, SUBSTRATUM_PACKAGE_NAME)) {
             return (!isPermitted!!)
@@ -128,7 +126,7 @@ object ThemeFunctions {
 
     fun isCallingPackageAllowed(packageId: String): Boolean {
         if (packageId == SUBSTRATUM_PACKAGE_NAME) return true
-        if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             OTHER_THEME_SYSTEMS.filter { packageId == it }.forEach { return true }
         }
         return false

skittles9823/TelegramXMods

app/src/main/kotlin/mods/skittles/telegramx/ThemeFunctions.kt
@@ -9,13 +9,12 @@ import android.content.pm.Signature
 import android.net.Uri
 import android.os.RemoteException
 import android.widget.Toast
-import mods.skittles.telegramx.Constants.BLACKLISTED_APPLICATIONS
-import mods.skittles.telegramx.Constants.ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS
-import mods.skittles.telegramx.Constants.MINIMUM_SUBSTRATUM_VERSION
-import mods.skittles.telegramx.Constants.OTHER_THEME_SYSTEMS
 
+import mods.skittles.telegramx.AdvancedConstants.BLACKLISTED_APPLICATIONS
+import mods.skittles.telegramx.AdvancedConstants.MINIMUM_SUBSTRATUM_VERSION
+import mods.skittles.telegramx.AdvancedConstants.OTHER_THEME_SYSTEMS
 
-@Suppress("ConstantConditionIf") // This needs to be defined by the themer, so suppress!
+@Suppress("ConstantConditionIf")
 object ThemeFunctions {
 
     val SUBSTRATUM_PACKAGE_NAME = "projekt.substratum"
@@ -40,7 +39,7 @@ object ThemeFunctions {
             if (pi.signatures != null
                     && pi.signatures.size == 1
                     && ((SIGNATURES[0] == pi.signatures[0]) ||
-                    (SIGNATURES[1] == pi.signatures[0]))) {
+                            (SIGNATURES[1] == pi.signatures[0]))) {
                 return true
             }
             false
@@ -90,7 +89,7 @@ object ThemeFunctions {
     }
 
     fun getSelfVerifiedIntentResponse(context: Context): Int? {
-        return if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        return if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             getSelfSignature(context)
         } else {
             getSubstratumSignature(context)
@@ -118,7 +117,7 @@ object ThemeFunctions {
 
     fun getSelfVerifiedThemeEngines(context: Context): Boolean? {
         val isPermitted: Boolean? = OTHER_THEME_SYSTEMS.any { isPackageInstalled(context, it) }
-        if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             return isPermitted
         } else if (isPackageInstalled(context, SUBSTRATUM_PACKAGE_NAME)) {
             return (!isPermitted!!)
@@ -128,7 +127,7 @@ object ThemeFunctions {
 
     fun isCallingPackageAllowed(packageId: String): Boolean {
         if (packageId == SUBSTRATUM_PACKAGE_NAME) return true
-        if (ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS) {
+        if (BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS) {
             OTHER_THEME_SYSTEMS.filter { packageId == it }.forEach { return true }
         }
         return false