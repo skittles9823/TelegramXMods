4307701cf374b276149cb3336af991ce376e229b
Skittles9823 <skat1@live.com.au>
2018-08-08 16:05:07 +1000

    Only post playstore builds to the dev group

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
-#cp app/build/outputs/apk/release/app-release.apk TelegramXMods-$TRAVIS_BUILD_NUMBER.apk
-curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="-1001386574981" -F sticker="CAADBQADPQADTBCSGr060PvRNzIFAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
-curl -F chat_id="-1001386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001386574981" -F document=@"TelegramXMods-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-Playstore-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
 echo $(./scripts/changelog.sh)
