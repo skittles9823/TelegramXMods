55e07e379e08d2387d005d4f899c50ec21985071
Rhyse Simpson <skittles9832@gmail.com>
2019-09-09 14:30:37 +1000

    10000 IQ

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -4,5 +4,5 @@ BUILD_SHA_SHORT=$(git rev-parse --short ${BUILD_SHA})
 #curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-ci-${BUILD_SHA_SHORT}.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="-1001386574981" -F sticker="CAADBQADPQADTBCSGr060PvRNzIFAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
-curl -F chat_id="-1001386574981" -F document=@"TelegramXMods-ci-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001386574981" -F document=@"TelegramXMods-ci-${BUILD_SHA_SHORT}.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 #curl -F chat_id="-1001386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
