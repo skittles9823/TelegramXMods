a7ff63edb014032186ef3170d88a19764b57746a
Rhyse Simpson <skittles9832@gmail.com>
2019-09-09 14:24:03 +1000

    Update deploy-telegram.sh

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+BUILD_SHA_SHORT=$(git rev-parse --short ${BUILD_SHA})
 #curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 #curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F document=@"TelegramXMods-ci-${BUILD_SHA_SHORT}.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
